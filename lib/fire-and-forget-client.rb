f58b44e2044460c887df5d7964d7a555b1707dfc
Ted Kulp <ted@tedkulp.com>
2012-12-03 15:56:26 -0500

    Initial commit

---
+++ lib/fire-and-forget-client.rb
@@ -0,0 +1,11 @@
+require "fire-and-forget-client/version"
+
+module Fire
+  module And
+    module Forget
+      module Client
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/fire-and-forget-client/version.rb
@@ -0,0 +1,9 @@
+module Fire
+  module And
+    module Forget
+      module Client
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
