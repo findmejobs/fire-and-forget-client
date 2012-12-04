4b204935e9f168ddeeb2358dbeaa6835036b82da
Ted Kulp <ted@tedkulp.com>
2012-12-03 20:28:04 -0500

    Finish specs

--- lib/fire-and-forget-client/client.rb
+++ lib/fire-and-forget-client/client.rb
@@ -13,20 +13,29 @@ module FireAndForget
     end
 
     def record_action(object_type, object_id, object_details, action, action_details)
-      json_message = JSON.generate({
+      json_message = {
         objectType: object_type,
         objectId: object_id,
         objectDetails: object_details,
         action: action,
         actionDetails: action_details
-      })
-      send_message(json_message)
+      }
+      json_message[:passphrase] = @passphrase unless @passphrase.empty?
+      send_message(JSON.generate(json_message))
     end
 
     private
 
     def send_message(json_message)
-      socket.send(json_message, 0, @hostname, @port)
+      send_to_socket(json_message, 0, @hostname, @port)
+    end
+
+    def send_to_socket(json_message, num, hostname, port)
+      socket.send(json_message, num, hostname, port)
+    end
+
+    def socket
+      @openedSocket ||= UDPSocket.new
     end
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,26 +1,78 @@
 require 'fire-and-forget-client'
 require 'rspec'
+require 'rspec-spies'
+require 'json'
+require 'socket'
 
 describe 'client' do
-  it 'should initialize with hostname, port and passphrase parameters' do
-    client = FireAndForget::Client.new('thing', 12345, 'something')
-    client.hostname.should eq('thing')
-    client.port.should eq(12345)
-    client.passphrase.should eq('something')
+  describe '#initialize' do
+    it 'should initialize with hostname, port and passphrase parameters' do
+      client = FireAndForget::Client.new('thing', 12345, 'something')
+      client.hostname.should eq('thing')
+      client.port.should eq(12345)
+      client.passphrase.should eq('something')
+    end
+
+    it "should have a default hostname of 127.0.0.1" do
+      client = FireAndForget::Client.new
+      client.hostname.should eq('127.0.0.1')
+    end
+
+    it "should have a default port" do
+      client = FireAndForget::Client.new('thing')
+      client.port.should > 0
+    end
+
+    it "should allow passphrase to be optional" do
+      client = FireAndForget::Client.new('thing', 12345)
+      client.passphrase.should eq('')
+    end
   end
 
-  it "should have a default hostname of 127.0.0.1" do
-    client = FireAndForget::Client.new
-    client.hostname.should eq('127.0.0.1')
+  describe '#record_action' do
+    before(:each) do
+      @data = {
+        objectType: 'user',
+        objectId: 1,
+        objectDetails: JSON.generate({ something: 'here' }),
+        action: 'action',
+        actionDetails: JSON.generate({ somethingelse: 'there' })
+      }
+    end
+
+    it "converts the incoming to parameters to json and send them to send_message" do
+      client = FireAndForget::Client.new()
+      client.stub(:send_message).and_return(nil)
+      client.record_action(@data[:objectType], @data[:objectId], @data[:objectDetails], @data[:action], @data[:actionDetails])
+      client.should have_received(:send_message).with(JSON.generate(@data))
+    end
+
+    it "adds the passphrase to the outgoing json" do
+      client = FireAndForget::Client.new('127.0.0.1', 42314, '12345')
+      client.stub(:send_message).and_return(nil)
+      client.record_action(@data[:objectType], @data[:objectId], @data[:objectDetails], @data[:action], @data[:actionDetails])
+      data = @data.clone
+      data[:passphrase] = '12345'
+      client.should have_received(:send_message).with(JSON.generate(data))
+    end
   end
 
-  it "should have a default port" do
-    client = FireAndForget::Client.new('thing')
-    client.port.should > 0
-  end
+  describe '#send_message' do
+    before(:each) do
+      @data = {
+        objectType: 'user',
+        objectId: 1,
+        objectDetails: JSON.generate({ something: 'here' }),
+        action: 'action',
+        actionDetails: JSON.generate({ somethingelse: 'there' })
+      }
+    end
 
-  it "should allow passphrase to be optional" do
-    client = FireAndForget::Client.new('thing', 12345)
-    client.passphrase.should eq('')
+    it "should call socket.send" do
+      client = FireAndForget::Client.new('127.0.0.1', 42314)
+      client.stub(:send_to_socket).and_return(nil)
+      client.record_action(@data[:objectType], @data[:objectId], @data[:objectDetails], @data[:action], @data[:actionDetails])
+      client.should have_received(:send_to_socket).with(JSON.generate(@data), 0, '127.0.0.1', 42314)
+    end
   end
 end
